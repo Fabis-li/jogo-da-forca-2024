92556f6ac4095fba573401ce5cda7959575dca2c
Fabiano Silva Lima <fabiano_slima@hotmail.com>
2024-04-16 09:49:41 -0300

    estrutura cadastro

---
+++ Gestao-Equipamentos/GestaoEquipamentos/GestaoEquipamentos.ConsoleApp/CadastrarEquipamento.cs
@@ -0,0 +1,25 @@
+﻿namespace GestaoEquipamentos.ConsoleApp
+{
+    public class CadastrarEquipamento
+    {
+        Equipamentos equipamento = new Equipamentos();
+        public string[] ArmazenarDadosEquipamento;
+
+        public void ObterDados()
+        {
+            Console.WriteLine("Cadastro de Equipamentos\n");
+            Console.WriteLine("Registrando um novo Equipamento\n");
+            Console.Write("Digite o nome do equipamento: ");
+            equipamento.Nome = Console.ReadLine();
+
+            Console.Write("\nDigite o preço do equipamento: ");
+            equipamento.PrecoAquisicao = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("\nDigite o número de serie do equipamento: ");
+            equipamento.NumeroSerie = Console.ReadLine();
+            Console.Write("\nDigite a data de fabricação: ");
+            equipamento.DataFabricacao = Console.ReadLine();
+
+        }
+
+    }
+}
